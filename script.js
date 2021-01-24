e0e32abc28198b1e02d35dc4251745dbb59d3c5c
FedyaShi <shisteroff.fedor@yanex.ru>
2021-01-24 15:26:02 +0500

    ads123

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $("#add").click(function() {
+        $("#modal").hide();
+    });
+
+
+        $("#add").click(function() {
+            $("#modal").show();
+    });
+
+    $("#close").click(function() {
+        $("#modal").hide();
+    });
+});
